584459254a0a6571b556e6a77bb772b33e8d5d37
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-12-03 19:03:53 -0800

    Added manual and partition tests.

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -18,6 +18,7 @@
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -28,32 +29,109 @@ import java.util.Random;
  */
 public class UrlValidatorTest extends TestCase {
 
-   private boolean printStatus = false;
-   private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
-
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
    
-   
    public void testManualTest()
    {
-	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
+       //protocol://host.subdomain.topdomain/dir1/dir2/filename.ext
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       ArrayList<String> testurls = new ArrayList<String>();
+       System.out.println(urlVal.isValid("http://www.amazon.com"));
+
+       //everything else valid or empty, different schemes
+       testurls.add("http://www.google.com");
+       testurls.add("http:/www.google.com");
+       testurls.add("http:///www.google.com");
+       testurls.add("http:\\www.google.com");
+       testurls.add("https://www.google.com");
+       testurls.add("ftp://www.google.com");
+       testurls.add("123://www.google.com");
+       testurls.add("foo://www.google.com");
+       //everything else valid or empty, different sub-domain
+       testurls.add("http://123.google.com");
+       testurls.add("http://1.2.3.google.com");
+       testurls.add("http://!@#.google.com");
+       testurls.add("http://foo.google.com");
+       //everything else valid or empty, different domain
+       testurls.add("http://www.google.edu");
+       testurls.add("http://www.google.co.uk");
+       testurls.add("http://www.google.123");
+       testurls.add("http://www.google.bar");
+       testurls.add("http://www.aaaaaaaaaaaaaaa.com");
+       testurls.add("http://www. .com");
+       testurls.add("http://www.google. ");
+       testurls.add("http://192.128.1.1");
+       testurls.add("http://1.1.1.1");
+       testurls.add("http://11.11.11.11");
+       testurls.add("http://111.111.111.111");
+       testurls.add("http://1111.1111.1111.1111");
+       testurls.add("http://1.1.1.1.1");
+       //everything else valid or empty, different port
+       testurls.add("http://www.google.com:0");
+       testurls.add("http://www.google.com: ");
+       testurls.add("http://www.google.com:80");
+       testurls.add("http://www.google.com:423");
+       testurls.add("http://www.google.com:32767");
+       testurls.add("http://www.google.com:32766");
+       testurls.add("http://www.google.com:65535");
+       testurls.add("http://www.google.com:65536");
+       testurls.add("http://www.google.com:foo");
+       testurls.add("https://www.google.com:423");
+       testurls.add("https://www.google.com:80");
+       testurls.add("https://www.google.com:bar");
+       testurls.add("ftp://www.google.com:21");
+       testurls.add("ftp://www.google.com:80");
+       testurls.add("ftp://www.google.com:bar");
+       //everything else valid or empty, different path
+       testurls.add("http://www.google.com/foo");
+       testurls.add("http://www.google.com/foo/bar");
+       testurls.add("http://www.google.com/~foo");
+       testurls.add("http://www.google.com//foo");
+       testurls.add("http://www.google.com/foo/bar/123/foobar");
+       testurls.add("http://www.google.com/foo?bar");
+       testurls.add("http://www.google.com/foo$");
+       testurls.add("http://www.google.com/ /");
+       testurls.add("http://www.google.com\foo");
+       for(String url : testurls)
+       {
+           System.out.println(urlVal.isValid(url) + ": " + url);
+       }
    }
-   
-   
-   public void testYourFirstPartition()
-   {
-	   
-   }
-   
-   public void testYourSecondPartition(){
-	   
-   }
-   
+
+    public void testPartitions()
+    {
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+        System.out.println("Testing for http://");
+        System.out.println("http://www.google.com is " + urlVal.isValid("http://www.google.com"));
+        System.out.println("www.google.com is  " + urlVal.isValid("www.google.com"));
+
+        System.out.println("Testing for port number");
+        System.out.println("http://www.google.com:-1 is " + urlVal.isValid("http://www.google.com:-1"));
+        System.out.println("http://www.google.com:0 is " + urlVal.isValid("http://www.google.com:0"));
+        System.out.println("http://www.google.com:65535 is " + urlVal.isValid("http://www.google.com:65535"));
+        System.out.println("http://www.google.com:65536 is " + urlVal.isValid("http://www.google.com:65536"));
+
+        System.out.println("Testing for .com, .net, etc");
+        System.out.println("http://www.google is " + urlVal.isValid("http://www.google"));
+
+        System.out.println("Testing for www");
+        System.out.println("http://google.com is " + urlVal.isValid("http://google.com"));
+
+        System.out.println("testing for query handling");
+        System.out.println("http://www.google.com/search?q=google is " + urlVal.isValid("http://www.google.com/search?q=google"));
+        //Testing for ability to handle ?
+        System.out.println("http://www.google.com/searchq=google is " + urlVal.isValid("http://www.google.com/searchq=google"));
+
+        System.out.println("Testing IP addresses");
+        System.out.println("http://-1.-1.-1.-1 is " + urlVal.isValid("http://-1.-1.-1.-1"));
+        System.out.println("http://0.0.0.0 is " + urlVal.isValid("http://0.0.0.0"));
+        System.out.println("http://255.255.255.255 is " + urlVal.isValid("http://255.255.255.255"));
+        System.out.println("http://256.256.256.256 is " + urlVal.isValid("http://256.256.256.256"));
+    }
    
    public void testIsValid()
    {
@@ -96,13 +174,8 @@ public class UrlValidatorTest extends TestCase {
            String[] validPorts = getValidPorts();
            int randomPortIdx = random.nextInt(validPorts.length);
            url = validSchemes[randomIdxScheme] + validHosts[randomIdxHost] + validPorts[randomPortIdx];
-           assertTrue("Failed with port" + validPorts[randomPortIdx], urlValidator.isValid(url));
+           assertTrue("Failed with port " + validPorts[randomPortIdx], urlValidator.isValid(url));
        }
-   }
-   
-   public void testAnyOtherUnitTest()
-   {
-	   
    }
 
     public String[] getInvalidSchemes() {
